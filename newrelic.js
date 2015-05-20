efad5b30142e664c649da472d673a9ca22597e64
Filomeno Santos <filomenosantos@gmail.com>
2015-05-20 12:08:34 +0200

    newrelic added

---
+++ newrelic.js
@@ -0,0 +1,24 @@
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.defaults.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+  /**
+   * Array of application names.
+   */
+  app_name: ['badgesland'],
+  /**
+   * Your New Relic license key.
+   */
+  license_key: 'b5b009d360cbb7853a31a4d50ccbb729e7431216',
+  logging: {
+    /**
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level: 'info'
+  }
+}
